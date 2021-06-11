18f7354a476b0e0fb0525224d805fc85c11e7d5b
a1barif2h <mohammadarifhossain80@gmail.com>
2021-06-11 21:29:48 +0600

    react native list and scrollbar

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Arif')
-  const [age, setAge] =useState(29)
-
-  const handleState = () => {
-    setName('Mohammad Arif Hossain')
-    setSon({name: 'Mohammad Arif Hossain', age: 8.5})
-  }
+  const [peoples, setPeoples] = useState([
+    {name: 'Arif', key: 1},
+    {name: 'Semi', key: 2},
+    {name: 'Araf', key: 3},
+    {name: 'Sajib', key: 4},
+    {name: 'Asif', key: 5},
+    {name: 'Anis', key: 6},
+    {name: 'Hosneara', key: 7},
+    {name: 'Arif', key: 8},
+    {name: 'Semi', key: 9},
+    {name: 'Araf', key: 10},
+    {name: 'Sajib', key: 11},
+    {name: 'Asif', key: 12},
+    {name: 'Anis', key: 13},
+    {name: 'Hosneara', key: 14},
+  ])
   return (
     <View style={styles.container}>
-      <Text style={styles.textSize}>Name: {name}, Age: {age} </Text>
-      <Text>Enter Name:</Text>
-      <TextInput onChangeText={(val) => setName(val)} placeholder='e.g. Jhon Due' style={styles.textInput} />
-      <Text>Enter Age:</Text>
-      <TextInput keyboardType='numeric' onChangeText={(val) => setAge(val)} placeholder='e.g. 99' style={styles.textInput} />
+      <ScrollView>
+        {
+          peoples.map(item => (
+            <View style={styles.list} key={item.key}>
+              <Text> {item.name} </Text>
+            </View>
+          ))
+        }
+      </ScrollView>
     </View>
   );
 }
@@ -24,17 +37,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+    marginHorizontal: 20
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  textSize: {
-    fontSize: 20
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#777',
-    width: 200,
-    margin: 20,
-    padding: 10
+  list: {
+    fontSize: 20,
+    padding: 30,
+    marginTop: 20,
+    backgroundColor: 'pink'
   }
 });
